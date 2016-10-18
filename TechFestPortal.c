8a946fb432f673defc1d9b49d82759f29650cd7c
Monal5031 <monalshadi16@gmail.com>
2016-10-18 15:55:55 +0530

    Updated TechFestPortal
    File having main() and where the actual project starts.

--- TechFestPortal.c
+++ TechFestPortal.c
@@ -6,21 +6,24 @@ void fordelay(int j)
     for(i=0;i<j;i++)
          k=i;
 }
-int main()
+void mainMenu()
     {
     int choice,i=0;
     printf(("\n\n\n\tEnter which one you want to select:\n\n"));
     printf("\t1.Admin Login\n");
     printf("\t2.User Login\n");
-    printf("\t3.Exit\n");
+    printf("\t3.Exit\n\tEnter:");
     scanf("%d",&choice);
-    printf("\tOk! You chose choice no.:%d\n\tLoading",choice);
+    printf("\tOk! You chose choice no.:%d",choice);
+    fordelay(100000000);
+    system("cls");
+    printf("Loading");
     for(i=0;i<=6;i++)
         {
             fordelay(100000000);
             printf(".");
         }
-
+    system("cls");
     switch(choice)
     {
     case 1:
@@ -30,7 +33,11 @@ int main()
         //userMenu();
         break;
     case 3:
-        printf("\n\n\tThanks for visiting! Please visit again!");
+        printf("\n\n\tThanks for visiting! Please visit again!\n\n");
     }
+    }
+int main()
+    {
+    mainMenu();
     return 0;
     }
