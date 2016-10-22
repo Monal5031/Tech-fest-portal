988bc49482c39cfdb1236ca48eba977cfeb1d1bf
Monal5031 <monalshadi16@gmail.com>
2016-10-23 00:23:36 +0530

    Menu for Admin!

---
+++ adminMenu.c
@@ -0,0 +1,19 @@
+
+void adminMenu()
+    {
+        int passer=0;
+
+        passer=authenticate(1,"AdminWillPass");
+
+            if(passer==0)
+                {
+                    system("cls");
+                    printf("\n\tLimit Exceeded!! \n\tTry Again!!");
+                    return;
+                }
+            else
+                {
+
+                }
+
+    }
