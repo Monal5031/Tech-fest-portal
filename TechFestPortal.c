c9ccf0ef390c9596daa07756640420efb71a352e
Monal5031 <monalshadi16@gmail.com>
2016-10-23 00:24:34 +0530

    Updates: adminMenu() and authenticate()

--- TechFestPortal.c
+++ TechFestPortal.c
@@ -1,11 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<windows.h>
 void fordelay(int j)
 {   int i,k;
     for(i=0;i<j;i++)
          k=i;
 }
+
+
+int authenticate(int caser,char pass[])
+    {
+        int passer=0,count=4;
+        char userpass[13];
+    switch(caser)
+    {
+     case 1:
+            printf("\n\tEnter Admin Password:");
+            scanf("%s",userpass);
+            passer=strcmp(pass,userpass);
+            while(passer!=0&&count>=0)
+            {
+                system("cls");
+                printf("\n\tWrong Password\n\tEnter again:");
+                scanf("%s",userpass);
+                passer=strcmp(pass,userpass);
+                count--;
+            }
+            if(passer==0)
+                return 1;
+            else if(passer!=0||count==0)
+                return 0;
+            else
+                return 1;
+            break;
+    case 2:
+            printf("\n\tEnter User Password");
+            scanf("%s",userpass);
+            passer=strcmp(pass,userpass);
+            while(passer!=0&&count>=0)
+            {
+                system("cls");
+                printf("\n\tWrong Password\n\tEnter again:");
+                scanf("%s",userpass);
+                passer=strcmp(pass,userpass);
+                count--;
+            }
+            if(passer==0)
+                return 1;
+            else if(passer!=0||count==0)
+                return 0;
+            else
+                return 1;
+    }
+    }
+
+void adminMenu()
+    {
+        int passer=0;
+
+        passer=authenticate(1,"AdminWillPass");
+            if(passer==0)
+                {
+                    system("cls");
+                    printf("\n\tLimit Exceeded!! \n\tTry Again!!");
+                    return;
+                }
+            else
+                {
+
+                }
+
+    }
 void mainMenu()
     {
     int choice,i=0;
@@ -27,7 +93,7 @@ void mainMenu()
     switch(choice)
     {
     case 1:
-        //adminMenu();
+        adminMenu();
         break;
     case 2:
         //userMenu();
