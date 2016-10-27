b78443f7e2870ed3afd2bf28524fdee3e5aae007
Monal5031 <monalshadi16@gmail.com>
2016-10-27 16:57:46 +0530

    Only encrypt() without main()

--- encrypt.c
+++ encrypt.c
@@ -1,21 +1,6 @@
-#include<stdio.h>
-#include<string.h>
-
 void encrypt(char pass[])
 {
     int i;
     for(i=0;i<strlen(pass);i++)
             pass[i]=pass[i]+10;
-}
-
-int main()
-{
-    FILE *p;
-    p=fopen("AdminPassword.txt","r");
-    char pass[13];
-    fscanf(p,"%s",pass);
-    encrypt(pass);
-    printf("%s",pass);
-    fclose(p);
-    return 0;
 }
