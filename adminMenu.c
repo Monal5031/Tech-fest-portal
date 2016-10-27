2aad47199380954be96c3c3a5386c5c3a24f4fb8
Monal5031 <monalshadi16@gmail.com>
2016-10-27 16:54:58 +0530

    New updates!
    createEvent updated

--- adminMenu.c
+++ adminMenu.c
@@ -1,6 +1,6 @@
-
 void adminMenu()
     {
+        int j;
         FILE *adminpass;
         adminpass=fopen("AdminPassword.txt","r");
         int passer=0;
@@ -16,7 +16,41 @@ void adminMenu()
                 }
             else
                 {
-
+                    system("cls");
+                    int choice;
+                    printf("\n\n\t1.Create Event\n\t2.Modify Event Information\n\t3.Change Password\n\t4.Main Menu");
+                    printf("\n\tEnter your Choice:");
+                    scanf("%d",&choice);
+                    switch(choice)
+                    {
+                    case 1:
+                       // createEvent();
+                        break;
+                    case 2:
+                       // modifyEvent();
+                        break;
+                    case 3:
+                        changePassword(pass);
+                        break;
+                    case 4:
+                        system("cls");
+                        printf("Loading");
+                        for(j=0;j<=6;j++)
+                            {
+                            fordelay(100000000);
+                            printf(".");
+                            }
+                            mainMenu();
+                    }
                 }
             fclose(adminpass);
+            printf("\n\tDO you want to continue?\n\t1.Yes\n\t2.No\n\t");
+            scanf("%d",&j);
+            if(j==1)
+                mainMenu();
+            else
+            {
+                printf("\n\n\tThanks for visiting! Please visit again!\n\n");
+             return;
+            }
     }
