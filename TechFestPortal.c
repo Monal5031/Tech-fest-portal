23f9757f0c607880018cb762bcf479f546a5fcf5
Monal5031 <monalshadi16@gmail.com>
2016-10-28 23:38:33 +0530

    Main function to Run the program!
    createEvent() added

--- TechFestPortal.c
+++ TechFestPortal.c
@@ -3,6 +3,51 @@
 #include<string.h>
 
 
+void createEvent()
+{
+    char name[30];
+    system("cls");
+    printf("\n\tPlease Enter the name of event you want to create (Max Size 30): ");
+    inputInfo(name);
+    FILE *create;
+    FILE *registerevent;
+    registerevent=fopen("EventListInfo.txt","a");
+    fprintf(registerevent,"Name:%s\n",name);
+    printf("\n\tCongratulations! Event Created \n\tPlease Enter Following Information about the event\n\t");
+    char time[10];
+    printf("Enter Time(with am or pm) **(8 size max)**: ");
+    inputInfo(time);
+    fprintf(registerevent,"%s\n",time);
+    char info[1000];
+    printf("\n\tEnter some Info about the event:");
+    inputInfo(info);
+    fprintf(registerevent,"%s\n",info);
+    printf("\n\tCongratulations! Event Successfully Registered");
+    fclose(registerevent);
+    strcat(name,".txt");
+    create=fopen(name,"w");
+    fclose(create);
+}
+
+
+void inputInfo(char name[])
+{
+
+int i=0;
+    char ch;
+    do{
+        scanf("%c",&ch);
+        if(ch=='\n')
+            break;
+        name[i]=ch;
+        i++;
+    }
+    while(1);
+    name[i]='\0';
+return;
+}
+
+
 void changePassword(char pass[])
 {
     FILE *password;
@@ -13,6 +58,7 @@ void changePassword(char pass[])
     scanf("%s",pass);
     encrypt(pass);
     fprintf(password,"%s",pass);
+    fclose(password);
 }
 
 
