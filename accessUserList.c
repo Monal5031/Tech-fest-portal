662d687da46cb75c23594c9656f1127668596146
Monal Shadi <monalshadi16@gmail.com>
2016-11-03 03:39:45 +0530

    Function to access list of all users

--- accessUserList.c
+++ accessUserList.c
@@ -1,15 +1,8 @@
-#include<stdio.h>
-int main()
-{
-    accessUserList();
-    return 0;
-}
-
 void accessUserList()
 {
     FILE *access;
     char ch;
-    printf("\n\t********LIST OF ALL THE PARTICIPANTS IN THE TECHFEST*******");
+    printf("\n\t********LIST OF ALL THE PARTICIPANTS IN THE TECHFEST*******\n");
     access=fopen("UserList.txt","r");
     while(!feof(access))
     {
@@ -17,7 +10,7 @@ void accessUserList()
         printf("%c",ch);
     }
     fclose(access);
-    printf("\n\tWhere do you want to go?\n\t1.Main Menu\n\t2.Admin Menu\n\t3.Exit");
+    printf("\n\tWhere do you want to go?\n\t1.Main Menu\n\t2.Admin Menu\n\t");
     int choice;
     scanf("%d",&choice);
     switch(choice)
@@ -26,10 +19,7 @@ void accessUserList()
         mainMenu();
         break;
     case 2:
-        adminMenu();
+        adminMenu(1);
         break;
-    case 3:
-        system("cls");
-
     }
 }
