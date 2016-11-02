9b995d8018ef1031eddf7270ae0bb8a583c87f1c
Monal Shadi <monalshadi16@gmail.com>
2016-11-03 03:45:20 +0530

    Function to Create new Event in the Fest

--- createEvent.c
+++ createEvent.c
@@ -7,18 +7,39 @@ void createEvent()
     FILE *create;
     FILE *registerevent;
     registerevent=fopen("EventList.txt","a");
-    fprintf(registerevent,"\n\t-%s\n",name);
+    fprintf(registerevent,"\n\t-%s",name);
     printf("\n\tCongratulations! Event Created \n\tPlease Enter Following Information about the event\n\t");
     char time[10];
     printf("Enter Time(with am or pm without spacing) **(8 size max)**: ");
     scanf("%s",time);
     char info[1000];
     printf("\n\tEnter some Info about the event:");
-    scanf("%s",info);
+    fflush(stdin);
+    gets(info);
     fillEventInfo(name,time,info);
     printf("\n\tCongratulations! Event Successfully Registered");
     fclose(registerevent);
     strcat(name,".txt");
     create=fopen(name,"w");
     fclose(create);
+    printf("\n\tDO You want to continue?");
+    printf("\n\t1.Yes\n\t2.No\n\tEnter:");
+    int tp;
+    scanf("%d",&tp);
+    if(tp==1)
+    {
+        system("cls");
+        printf("\n\tWhere do you want to go?\n");
+        printf("1.Main Menu\n\t2.Admin Menu\n\t");
+        int choice;
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1:
+            mainMenu();
+            break;
+        case 2:
+            adminMenu(1);
+        }
+    }
 }
