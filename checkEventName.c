d54cb70ce5f5b13aeb6a4c0cdd68611720f8e424
Monal5031 <monalshadi16@gmail.com>
2016-11-11 20:35:53 +0530

    Function to check Event name

---
+++ checkEventName.c
@@ -0,0 +1,13 @@
+int checkEventName(char event[])
+{
+    FILE *ev;
+    ev=fopen("TEMPE.txt","r");
+    char check[30];
+    while(!feof(ev))
+    {
+        fscanf(ev,"%s",check);
+        if(strcmp(check,event)==0)
+            return 1;
+    }
+    return 0;
+}
